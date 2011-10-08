122b7f54b0665b52f24bb96a78d95bd2d186052c
darkdiplomat <nintendofreck@yahoo.com>
2011-10-07 21:53:40 -0500

    v1.3 Added Custom Messages and Delay, changed Activate to be for more than just "Admin", fixed exploding for b9.
    Signed-off-by: darkdiplomat <nintendofreck@yahoo.com>

--- SelfDestruct/src/SelfDestruct.java
+++ SelfDestruct/src/SelfDestruct.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -5,7 +10,7 @@ public class SelfDestruct extends Plugin {
 	SelfDestructListener listener = new SelfDestructListener();
 	static Logger log = Logger.getLogger("Minecraft");
 	String name = "SelfDestruct";
-	String version = "1.2";
+	String version = "1.3";
 	String author = "Darkdiplomat";
 
 	public void disable() {
@@ -14,6 +19,7 @@ public class SelfDestruct extends Plugin {
 	}
 
 	public void enable() {
+		createMessageFiles();
 		log.info(name + " version " + version + " by " + author + " enabled");
 	}
 	
@@ -21,5 +27,77 @@ public class SelfDestruct extends Plugin {
 		etc.getLoader().addListener(PluginLoader.Hook.COMMAND, listener, this, PluginListener.Priority.MEDIUM);
 		
 	}
-
+	
+	public void createMessageFiles(){
+		File Dir = new File("plugins/config/SelfDestruct");
+		//create SelfDestruct directory if it does not exits
+		if(!Dir.exists()){
+			Dir.mkdir();
+		}
+		//create SelfDestruct Message Files if they don't exist
+		File DudFile = new File(Dir + File.separator + "Dud.txt");
+		File LowPowerFile = new File(Dir + File.separator + "LowPower.txt");
+		File MediumPowerFile = new File(Dir + File.separator + "MediumPower.txt");
+		File HighPowerFile = new File(Dir + File.separator + "HighPower.txt");
+		//DudFile does not exist, create it
+		if(!DudFile.exists()){
+			try{
+				DudFile.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(DudFile));
+				writer.write("@3%player% @6was a dud!"); writer.newLine();
+				writer.write("@3%player% @6forgot to set the charge!"); writer.newLine();
+				writer.write("@3%player% @6deactivated the charge just in time!"); writer.newLine();
+				writer.write("@3%player% @6didn't know how to wire themself!"); writer.newLine();
+				writer.close();
+			} catch (IOException e) {
+				SelfDestruct.log.log(Level.SEVERE, "[SelfDestruct] Failed to create \"Dud.txt\"");
+			}
+		}
+		//LowPowerFile does not exist, create it
+		if(!LowPowerFile.exists()){
+			try{
+				LowPowerFile.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(LowPowerFile));
+				writer.write("@3%player% @dwasn't fully charged."); writer.newLine();
+				writer.write("@3%player% @6forgot to connect all the charges!"); writer.newLine();
+				writer.write("@3%player% @6had a lot of fake @cTNT@6 in the mix!"); writer.newLine();
+				writer.write("@3%player% @6thought @8GunPowder@6 would be enough!"); writer.newLine();
+				writer.write("@3%player% @6forgot to add @eSand@6 to the mix!"); writer.newLine();
+				
+				writer.write(""); writer.newLine();
+				writer.close();
+			} catch (IOException e) {
+				SelfDestruct.log.log(Level.SEVERE, "[SelfDestruct] Failed to create \"LowPower.txt\"");
+			}
+		}
+		//MediumFile does not exist, create it
+		if(!MediumPowerFile.exists()){
+			try{
+				MediumPowerFile.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(MediumPowerFile));
+				writer.write("@3%player% @6has selfdestructed!"); writer.newLine();
+				writer.write("@3%player% @6thought @cTNT@6 was safe!"); writer.newLine();
+				writer.write("@3%player% @6tried being a @7Ghast Fireball@6!"); writer.newLine();
+				writer.write("@3%player% @6dropped a stack of @cTNT@6!"); writer.newLine();
+				writer.write("@3%player% @6mixed @eSand@6 with their @8GunPowder@6!"); writer.newLine();
+				writer.close();
+			} catch (IOException e) {
+				SelfDestruct.log.log(Level.SEVERE, "[SelfDestruct] Failed to create \"MediumPower.txt\"");
+			}
+		}
+		//HighFile does not exist, create it
+		if(!HighPowerFile.exists()){
+			try{
+				HighPowerFile.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(HighPowerFile));
+				writer.write("@3%player% @4has become one with the @aCreepers@4!"); writer.newLine();
+				writer.write("@3%player% @4tried taking everything out!"); writer.newLine();
+				writer.write("@3%player% @4used a little too much!"); writer.newLine();
+				writer.write("@3%player% @4has been blown sky high!"); writer.newLine();
+				writer.close();
+			} catch (IOException e) {
+				SelfDestruct.log.log(Level.SEVERE, "[SelfDestruct] Failed to create \"HighPower.txt\"");
+			}
+		}
+	}
 }
--- SelfDestruct/src/SelfDestructListener.java
+++ SelfDestruct/src/SelfDestructListener.java
@@ -1,127 +1,146 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 
 public class SelfDestructListener extends PluginListener{
 	static Logger log = Logger.getLogger("Minecraft");
 	Server server = etc.getServer();
+	Timer countdown;
+	ArrayList<Player> blowingup = new ArrayList<Player>();
+	ArrayList<String> powermessage;
+	
+	
 	public boolean onCommand(Player player, String[] split){
+		int delay = 3;
 		if (split[0].equals("/selfdestruct") && player.canUseCommand("/selfdestruct")){
-			boolean inprogress = false;
-			if (inprogress == false){
-				inprogress = true;
-				int i = -1;
-				while (i < 2){
-					i++;
-					server.messageAll("§3" + player.getName() + " §cwill selfdestruct in §e" + (3-i) + " §cseconds");
+			if (!blowingup.contains(player) ){
+				countdown = new Timer();
+				blowingup.add(player);
+				if (split.length > 1){
 					try{
-						Thread.currentThread();
-						Thread.sleep(1000);
-					}catch(InterruptedException ie){
-						log.info("[SelfDestruct] Something broke the timer...");
-						server.messageAll("§c?");
-						return true;
+						delay = Integer.parseInt(split[1]);
+					}catch (NumberFormatException nfe){
+						player.sendMessage("§e"+split[1]+"§c is not a number!");
+						player.sendMessage("§cDelay auto-set to §e3 seconds");
+						delay = 3;
 					}
 				}
-				Random explode = new Random();
-				Integer boom = explode.nextInt(3);
-				String booomed = boom.toString();
-				float exploded = Float.parseFloat(booomed);
-				player.getWorld().explode(player, player.getX(), player.getY(), player.getZ(), exploded);
-				player.sendMessage(booomed);
-				int rm = explode.nextInt(3);
-				if (boom == 0){
-					if (rm == 1){
-						server.messageAll("§3" + player.getName() + " §6was a dud.");
-						inprogress = false;
-						return true;
-					}else if (rm == 2){
-						server.messageAll("§3" + player.getName() + " §6forgot to set the charge.");
-						inprogress = false;
-						return true;
-					}else{
-						server.messageAll("§3" + player.getName() + " §6deavtivated the charge just in time.");
-						inprogress = false;
-						return true;
-					}
-				}else if (boom == 1){
-					server.messageAll("§3" + player.getName() + " §dwasn't fully charged.");
-					inprogress = false;
-					return true;
-				}else{
-					if (rm == 2){
-						server.messageAll("§3" + player.getName() + " §4has selfdestructed!");
-						inprogress = false;
-						return true;
-					}else if (rm == 1){
-						server.messageAll("§3" + player.getName() + " §4has become one with the §aCreepers§4!");
-						inprogress = false;
-						return true;
-					}else{
-						server.messageAll("§3" + player.getName() + " §4has been blown sky high!");
-						inprogress = false;
-						return true;
-					}
-				}
+				server.messageAll("§3" + player.getName() + " §cwill selfdestruct in §e" + delay + " §cseconds");
+				countdown.schedule(new TheFinalCountDown(player, delay, delay), 1000);
+				return true;
 			}else{
-				return false;
+				player.sendMessage("§cYou are already activated!");
+				return true;
 			}
-		}else if (player.isAdmin() && split[0].equals("/activate") && (split.length > 1)){
-			Player detonated = etc.getServer().getPlayer(split[1]);
-			if (detonated != null){
-				int i = -1;
-				server.messageAll("§3" + player.getName() + "§c activated §3" + detonated.getName());
-				while (i < 2){
-					i++;
-					server.messageAll("§3" + detonated.getName() + " §cwill selfdestruct in §e" + (3-i) + " §cseconds");
-					try{
-						Thread.currentThread();
-						Thread.sleep(1000);
-					}catch(InterruptedException ie){
-						log.info("[SelfDestruct] Something broke the timer...");
-						server.messageAll("§c?");
-						return true;
-					}
-				}
-				Random explode = new Random();
-				Integer boom = explode.nextInt(3);
-				String booomed = boom.toString();
-				float exploded = Float.parseFloat(booomed);
-				detonated.getWorld().explode(detonated, player.getX(), player.getY(), player.getZ(), exploded);
-				detonated.sendMessage(booomed);
-				int rm = explode.nextInt(3);
-				if (boom == 0){
-					if (rm == 1){
-						server.messageAll("§3" + detonated.getName() + " §6was a dud.");
-						return true;
-					}else if (rm == 2){
-						server.messageAll("§3" + detonated.getName() + " §6forgot to set the charge.");
-						return true;
+		}else if (split[0].equals("/activate") && player.canUseCommand("/activate")){
+			if (split.length > 1){
+				Player activated = etc.getServer().getPlayer(split[1]);
+				if (activated != null){
+					if (!activated.canUseCommand("/noactivate")){
+						if (!blowingup.contains(activated)){
+							countdown = new Timer();
+							blowingup.add(activated);
+							if (split.length > 2){
+								try{
+									delay = Integer.parseInt(split[2]);
+								}catch (NumberFormatException nfe){
+									player.sendMessage("§e"+split[2]+"§c is not a number!");
+									player.sendMessage("§cDealy auto-set to §e3 seconds");
+									delay = 3;
+								}
+							}
+							server.messageAll("§3"+activated.getName()+" §cwill selfdestruct in §e"+delay+" §cseconds");
+							countdown.schedule(new TheFinalCountDown(activated, delay, delay), 1000);
+							return true;
+						}else{
+							player.sendMessage("§3" + split[1] + "§c is already activated!");
+							return true;
+						}
 					}else{
-						server.messageAll("§3" + detonated.getName() + " §6deavtivated the charge just in time.");
-						return true;
+						player.sendMessage("§3"+split[1]+"§c cannot be activated by others!");
 					}
-				}else if (boom == 1){
-					server.messageAll("§3" + detonated.getName() + " §dwasn't fully charged.");
-					return true;
 				}else{
-					if (rm == 2){
-						server.messageAll("§3" + detonated.getName() + " §4has selfdestructed!");
-						return true;
-					}else if (rm == 1){
-						server.messageAll("§3" + detonated.getName() + " §4has become one with the §aCreepers§4!");
-						return true;
-					}else{
-						server.messageAll("§3" + detonated.getName() + " §4has been blown sky high!");
-						return true;
-					}
+					player.sendMessage("§3" + split[1] + "§c is not online...");
+					return true;
 				}
 			}else{
-				player.sendMessage("§3" + split[1] + "§c is not online...");
+				player.sendMessage("§cUsage is: /activate <Player> [delay]");
 				return true;
 			}
 		}
 		return false;
 	}
-
+	public void Detonate(Player player){
+		blowingup.remove(player);
+		if (player.isConnected()){
+			Random explode = new Random();
+			Integer power = explode.nextInt(4);
+			Mob creeper = new Mob("Pig");
+			player.getWorld().explode(creeper, player.getX(), player.getY(), player.getZ(), (float)power);
+			server.messageAll(DetMessage(power, player));
+		}else{
+			server.messageAll("§3"+player.getName()+"§b disconnected before they exploded!");
+		}
+	}
+	public String DetMessage(int power, Player player){
+		powermessage = new ArrayList<String>();
+		Random random = new Random();
+		String Detmessage = "";
+		String Dir = "plugins/config/SelfDestruct/";
+		String FileLoc = "";
+		if (power == 0){
+			FileLoc = "Dud.txt";
+		}else if (power == 1){
+			FileLoc = "LowPower.txt";
+		}else if (power == 2){
+			FileLoc = "MediumPower.txt";
+		}else if (power >= 3){
+			FileLoc = "HighPower.txt";
+		}
+		try{
+			BufferedReader in = new BufferedReader(new FileReader(Dir + FileLoc));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		    	powermessage.add(str);
+		    }
+		    in.close();
+		}catch (IOException ioe){
+			log.info(ioe.toString());
+		}
+		int message = random.nextInt(powermessage.size() -1);
+		String predet = powermessage.get(message);
+		Detmessage = predet.replace("@", "§").replace("%player%", player.getName());
+		
+		return Detmessage;
+	}
+	public class TheFinalCountDown extends TimerTask{
+		Player player;
+		int remaining, delay;
+		
+		public TheFinalCountDown(Player activated, int remaining, int delay){
+			this.player = activated;
+			this.remaining = remaining;
+			this.delay = delay;
+		}
+		public void run() {
+			remaining--;
+			if (remaining > 0){
+				if (player.isConnected()){
+					server.messageAll("§3"+player.getName()+" §cwill selfdestruct in §e"+remaining+" §cseconds");
+					countdown.schedule(new TheFinalCountDown(player, remaining, delay), 1000);
+				}else{
+					server.messageAll("§3"+player.getName()+"§b disconnected before they exploded!");
+					blowingup.remove(player);
+				}
+			}else{
+				Detonate(player);
+			}
+		}
+	}
 }
